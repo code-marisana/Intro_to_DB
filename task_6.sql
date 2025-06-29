a180c35e6f5537c07643795bd2fbbb8808de734a
code-marisana <marisana@marisanamasha.com>
2025-06-29 14:37:02 +0200

    adsf

--- task_6.sql
+++ task_6.sql
@@ -8,9 +8,9 @@ INSERT INTO customer (
     (3,
     "Obed Ehoneah",
     "eobed@sandtech.com",
-    "125 Happiness Ave."),
+    "125 Happiness  Ave."),
     (4,
     "Nehemial Kamolu",
     "nkamolu@sandtech.com",
-    "126 Happiness Ave."),
+    "126 Happiness  Ave."),
 );
