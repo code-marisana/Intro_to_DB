d783a3dd1be36a8ae5b25796dc06115fd50ec164
code-marisana <marisana@marisanamasha.com>
2025-06-29 13:25:18 +0200

    add datatypes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,41 +1,47 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
 
+-- Authors table
 CREATE TABLE Authors (
-    author_id,
+    author_id INT AUTO_INCREMENT,
     author_name VARCHAR(215),
     PRIMARY KEY (author_id)
 );
 
+-- Books table
 CREATE TABLE Books (
-    book_id INT,
+    book_id INT AUTO_INCREMENT,
     title VARCHAR(130),
-    author_id
+    author_id INT,
     price DOUBLE,
     publication_date DATE,
     PRIMARY KEY (book_id),
     FOREIGN KEY (author_id) REFERENCES Authors (author_id)
 );
 
+-- Customers table
 CREATE TABLE Customers (
-    customer_id,
+    customer_id INT AUTO_INCREMENT,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT,
     PRIMARY KEY (customer_id)
 );
 
+-- Orders table
 CREATE TABLE Orders (
-    order_id,
-    customer_id,
+    order_id INT AUTO_INCREMENT,
+    customer_id INT,
     order_date DATE,
     PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
+-- Order_Details table
 CREATE TABLE Order_Details (
-    orderdetailid,
-    order_id,
-    book_id,
+    orderdetailid INT AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
     quantity DOUBLE,
     PRIMARY KEY (orderdetailid),
     FOREIGN KEY (order_id) REFERENCES Orders (order_id),
