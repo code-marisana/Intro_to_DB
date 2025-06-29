17a9d569c0cb2eb10cf2558bee300ef490af0293
code-marisana <marisana@marisanamasha.com>
2025-06-29 13:28:42 +0200

    remove spaces

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -44,6 +44,6 @@ CREATE TABLE Order_Details (
     book_id INT,
     quantity DOUBLE,
     PRIMARY KEY (orderdetailid),
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
