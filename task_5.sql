aa252b1d62c8e1df977db5586e93bd7c54ef66dc
code-marisana <marisana@marisanamasha.com>
2025-06-29 14:23:06 +0200

    ADD INSERT

---
+++ task_5.sql
@@ -0,0 +1,11 @@
+INSERT INTO table (
+    customer_id,
+    customer_name,
+    email,
+    address    
+) VALUES (
+    1,
+    "Cole Baidoo",
+    "cbaidoo@sandtech.com",
+    "123 Happiness Ave"
+);
